646aa23b853413626001fc61cf91c30703867c8d
Ismail-Mokoena <ismailmokoena@icloud.com>
2023-05-18 19:33:28 -0600

    crossover

--- individual.py
+++ individual.py
@@ -5,14 +5,36 @@ from random import random
 class Individual():
     space: list = field(default_factory=list)
     prices: list = field(default_factory=list)
-    space_limit: int = 0
+    space_limit: float = 0.0
+    score_eval: int =0
+    used_space: float = 0.0
     generation: int = 0
     chromosome: list = field(default_factory=list)
     
 
     def set_chromosome(cls):
+        """Decides randomly which product we'll load
+            0- We dont select, 1- We select."""
         for i in range(len(cls.space)):
             if random() < 0.5:
                 cls.chromosome.append('0')
             else:
                 cls.chromosome.append('1')
+    
+    def fitness(cls):
+        score = 0
+        total_space = 0
+        for i in range(len(cls.chromosome)):
+            if cls.chromosome[i] == '1':
+                score += float(cls.prices[i])
+                total_space+=float(cls.space[i])
+        
+        if total_space>cls.space_limit:
+            cls.score=1
+        cls.score_eval=score
+        cls.used_space=total_space
+        
+    def crossover(cls, parent):
+        cutoff = round(random()*len(cls.chromosome))
+        chromosome_a = parent.chromosome[0:cutoff] + cls.chromosome[cutoff::]
+        chromosome_b = cls.chromosome[0:cutoff] + parent.chromosome[cutoff::]
--- main.py
+++ main.py
@@ -6,27 +6,39 @@ def load_products():
             for line in file:
                 items_list = line.rstrip().split(',')                
                 yield Product(items_list[0],items_list[1],items_list[2])
+
         
-def individuals()-> Individual:
+def print_item(chromosome_list, product_list):
+    for i in range(len(chromosome_list)):
+        if chromosome_list[i]=="1":
+            print(product_list[i])
+   
+        
+
+
+if __name__=="__main__":
     space_list = []
     price_list = []
+    product_list = []
     limit = 3
+    
     for each in load_products():
         space_list.append(each.space)
         price_list.append(each.price)
-    x = Individual(space_list, price_list, limit)
-    x.set_chromosome()
-    return x
-        
-
-if __name__=="__main__":
+        product_list.append(each.name)
     
-    i1 = individuals()
-    print("Spaced", i1.space)
-    print("chromosome", i1.chromosome)
-    
-    
-    #print(load_products())
+    #first generation
+    #A 
+    parent_a  = Individual(space_list, price_list, limit)
+    parent_a.set_chromosome()
+    parent_a.fitness()
+    print_item(parent_a .chromosome, product_list)
+    #B
+    parent_b  = Individual(space_list, price_list, limit)
+    parent_b.set_chromosome()
+    parent_b.fitness()
+    print("\nB:")
+    print_item(parent_b.chromosome, product_list)
     
     
    
